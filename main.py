2df232ad5161e0b942c79122c289cf39e02a61fb
Thuy Nguyen <thuicide@gmail.com>
2022-10-10 02:04:57 -0700

    Chap7 Lab1

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+# List for 5 int values
+numbers = []
 
-# ******************************
-# Make your Code
-# ******************************
+# Get 5 int values from user
+i = 0
+print("Enter 5 integer values: ")
+while i < 5:
+    num = int(input(""))
+    numbers.append(num)
+    i += 1
+
+# Get sum - (max + min) of values in list
+sum = sum(numbers) - (min(numbers) + max(numbers))
+
+# Print sum - (max + min)
+print("List:", numbers)
+print("Min value:", min(numbers))
+print("Max value:", max(numbers))
+print("Sum - max/min:", sum)
